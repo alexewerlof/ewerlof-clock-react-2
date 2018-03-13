18f5f63d96c1c8a58c9f239ab334ef660524c1cb
Alex Ewerlöf <alex.ewerlof@schibsted.com>
2018-03-14 00:07:27 +0100

    now we got indicators

--- src/Face.js
+++ src/Face.js
@@ -1,4 +1,20 @@
 import React, { Component } from 'react';
+import { minute2deg } from './util'
+
+function Indicator({ cPoly, n, color }) {
+    const big = n % 5 === 0
+    const length = big ? 72 : 88
+    const width = big ? 6 : 2
+    const rotation = minute2deg(n);
+
+    return <line
+      x1={cPoly.X(rotation, length)}
+      y1={cPoly.Y(rotation, length)}
+      x2={cPoly.X(rotation, 95)}
+      y2={cPoly.Y(rotation, 95)}
+      stroke={color}
+      strokeWidth={cPoly.R(width)} />
+}
 
 export default class Face extends Component {
 
@@ -7,10 +23,11 @@ export default class Face extends Component {
     }
 
     render() {
-        return (
-        <div>
-            this is the face! {this.props.ms}
-        </div>
-        );
+
+        const indicators = []
+        for (let i = 0; i < 60; i++) {
+            indicators.push(<Indicator key={i} n={i} {...this.props} color={this.props.indicatorColor}/>)
+        }
+        return <g>{indicators}</g>
     }
 }
